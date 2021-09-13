d0fb6292c901fbe1917a2829706e9eb8f1a08aee
Saymon Coppi <saymoncoppi@gmail.com>
2021-09-13 14:46:50 -0300

    Update pdfff.sh
    - Fix errors on filename SPACES
    - Create Vars for folders
    - Move converted files to "converted folder"
    - Move used files to "bkp folder"
    - Count files and splash info

--- pdfff.sh
+++ pdfff.sh
@@ -8,7 +8,7 @@
 #------------------------------------------------------------------------------------------------------------------------
 #------------------------------------------------------------------------------------------------------------------------
 
-last_update='20210905'
+last_update='20210913'
 
 # Color Settings
 c1='\e[38;5;235m'       # Dark Grey
@@ -51,9 +51,22 @@ fontfix_text3=('Files not Found!' 'Nenhum arquivo encontrado!' 'Missing_ES_Trans
 fontfix_text4=('Original:' 'Original:' 'Missing_ES_Translation ')
 fontfix_text5=('New:' 'Novo:' 'Missing_ES_Translation ')
 fontfix_text6=('Compression:' 'Compressão:' 'Missing_ES_Translation ')
+fontfix_text7=('Files converted: ' 'Arquivos convertidos: ' 'Missing_ES_Translation ')
 
 #font fix texts
 check_env_text1=('Ops! You have to run' 'Ops! Você deve executar' 'Missing_ES_Translation ')
+
+#------------------------------------------------------------------------------------------------------------------------
+#------------------------------------------------------------------------------------------------------------------------
+#                   Folders
+#------------------------------------------------------------------------------------------------------------------------
+#------------------------------------------------------------------------------------------------------------------------
+# Dont use folders with SPACE
+INPUT="input/"
+OUTPUT="converted/"
+BACKUP="bkp/"
+LOG="logs/"
+
 
 
 #------------------------------------------------------------------------------------------------------------------------
@@ -80,27 +93,28 @@ function font_fix (){
     dColorImageDownsampleType="/Bicubic"
     dGrayImageDownsampleType="/Bicubic"
 
-    pdf_resolution="72"
+    pdf_resolution="72"                     # 72 is used to decrease the file size
     dColorImageResolution=$pdf_resolution
     dGrayImageResolution=$pdf_resolution
     dMonoImageResolution=$pdf_resolution
 
     # Documment properties
-    pdf_author="author"
-    pdf_producer="pdfff"
-    pdf_subject="pdfff repaired file"
+    pdf_author="author"                     # Fill here if you want
+    pdf_producer="pdfff"                    # Fill here if you want
+    pdf_subject="pdfff repaired file"       # Fill here if you want
 
 
 
     echo ${fontfix_text1[$lang]}
     echo "------------------------------------------------------------------------------------------------------------------------"
-    
+    files_converted=0
     for i in "${input_folder[@]}"; do 
-        current_file=$(echo $i | awk -F/ '{print $2}')
+        current_file=$(echo "${i}" | awk -F/ '{print $2}')
         current_title=$(basename -- "${current_file}" .pdf)
-        
+     
         echo -ne "${current_file}   -   ${c4}${fontfix_text2[$lang]}${normal}   -   "
-        gs -q -o "output/${current_file}" \
+        
+        gs -q -o "${OUTPUT}/${current_file}" \
         -sDEVICE=$sDEVICE \
         -dPDFSETTINGS=$dPDFSETTINGS \
         -dAutoRotatePages=$dAutoRotatePages \
@@ -116,14 +130,18 @@ function font_fix (){
         /Subject (${pdf_subject})
         /DOCINFO pdfmark" \ -f
 
-
-        original_size=$(wc -c "input/${current_file}" | awk '{print $1}')
-        post_size=$(wc -c "output/${current_file}" | awk '{print $1}')
+        original_size=$(wc -c "${INPUT}${current_file}" | awk '{print $1}')
+        post_size=$(wc -c "${OUTPUT}${current_file}" | awk '{print $1}')
         compression_ratio=$(echo "scale=4 ; (($post_size-$original_size)/$original_size)*100" | bc | awk -F. '{print $1}')
         echo -ne "${fontfix_text4[$lang]}${c4}${original_size} ${normal}${fontfix_text5[$lang]}${c4}$post_size ${normal}${fontfix_text6[$lang]}${c4}$compression_ratio%${normal}\n"
 
-        echo -e "$(date '+%Y%m%d%H%M%S');$current_file, $original_size, $post_size, $compression_ratio"  |& tee >> logs/$OUTPUTFILE
+        echo -e "$(date '+%Y%m%d%H%M%S');$current_file, $original_size, $post_size, $compression_ratio"  |& tee >> "$LOG/$OUTPUTFILE"
+
+        mv "${INPUT}${current_file}" "${BACKUP}${current_file}"
+
+        files_converted=`expr $files_converted + 1`
     done
+    echo -ne "\n${fontfix_text7[$lang]}${c4}$files_converted\n"
 }
 
 
@@ -145,18 +163,18 @@ function check_env (){
     done
 
     #check folders
-	for folder in input output logs; do
+	for folder in $INPUT $BACKUP $OUTPUT $LOG; do
 		[[ ! -d $folder ]] && mkdir -p $folder	
 	done
 
-    count_input_files=$(ls -1q input/*.pdf | wc -l)
+    count_input_files=$(ls -1q ${INPUT}*.pdf | wc -l)
 
     #check input content 
     if [ $count_input_files -gt 0 ]
     then
         OUTPUTFILE="$(date '+%Y%m%d%H%M%S').txt"
         
-        readarray -t input_folder <<<"$(ls input/*.pdf)"
+        readarray -t input_folder <<<"$(ls ${INPUT}*.pdf)"
         font_fix
 else 
     echo ${fontfix_text3[$lang]}
